5529a1bff5dbbaf5503884fa9562b44d45bc6ac9
javierrazo <javier.razo@uabc.edu.mx>
2020-07-24 20:20:31 -0700

    First commit

---
+++ src/build/precache-manifest.d49e321b7a55a581ae4962ae5d6ac804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d412954c08ad8b23ac9574869e4b8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "145f9f498d82806d3e76",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0f32a4011bac3f53b0bf",
+    "url": "/static/css/main.38f5747e.chunk.css"
+  },
+  {
+    "revision": "145f9f498d82806d3e76",
+    "url": "/static/js/2.f52e08cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f52e08cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f32a4011bac3f53b0bf",
+    "url": "/static/js/main.6a0292b5.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
